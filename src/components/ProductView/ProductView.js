ea8e88caeac19f86bc8403a0097639a356d26bab
Your Name <you@example.com>
2019-03-27 15:26:55 -0700

    deleted get_listing_by_id, edited product controller / product view, added a new route to HOME

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -7,44 +7,63 @@ class ProductView extends Component {
     constructor(props){
         super(props)
         this.state = {
-            listing_id: this.props.listing_id
+            listing_id: [{
+                description: "", 
+                email: "", 
+                listing_id: "",
+                listing_name: "",
+                price: "",
+                profile_name: "",
+                sold: false,
+                tags: "",
+                time_stamp: "",
+                type: "",
+                user_id: ""
+             }]
         }
     }
 
     // Need SQL file to get listing by ID# 
     // Deconstruct everything we need from the listing and place them in the appropriate divs 
      
-    componentDidMount = () => {
+    componentDidMount(){
+        console.log("PROPS", this.props)
         this.fetchListingID()
     }
 
     fetchListingID = () => {
-        axios.get(`/api/listings/${this.props.listing.id}`).then(response => {
+        axios.get(`/api/listings/${this.props.match.params.listing_id}`).then(response => {
             console.log("fetchListingID", response)
-            this.props.listing_id(response.data)
+            this.props.update_listing_id(response.data)
             this.setState({
                 listing_id: response.data
             })
-            console.log("listing_id", response.data)
+            console.log("listing_id", response.data, "DESCRIPTION", this.state)
         }) 
     }
 
     render(){
+        console.log(this.state.listing_id[0])
+        
         return(
 
             <div className="productView">
 
                 <div className="productView__images">
+                    {/* {this.state} */}
                     IMAGES
                 </div>
 
                 <div className="productView__descriptionAndUserInfo">
 
                     <div className="productView__description">
-                        DESCRIPTION 
+                    {this.state.listing_id.description}
+                    {this.state.listing_id[0].description}
+                        
                     </div>
 
                     <div className="productView__userInfo">
+                    {this.state.listing_id[0].profile_name}
                         USER INFO 
                     </div>
                 
