a4d37c11a4af04fb0381c9e501ed844a71fa9a20
Jagger Behm <jaggerbehm@Jaggers-MacBook-Pro.local>
2019-03-29 16:48:21 -0700

    user and footer

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {getUser} from "../../redux/reducer"
+import './user.css'
 
 class User extends Component {
     constructor (props){
@@ -10,9 +13,35 @@ class User extends Component {
     }
     render(){
         return (
-            <div className="user"></div>
+            <div className="user">
+                <div className="user__left">
+                    <div className="user__userInfo">
+                        <div className="user__image">picture</div>
+                        <div className="user__name">{this.props.user.profile_name}</div>
+                        <div className="user__rating">stars</div>
+                    </div>
+                    <div className="user__location">location</div>
+                </div>
+                <div className="user__right">
+                    <div className="user__items">
+                        <div className="user__itemImage">Image</div>
+                        <div className="user__itemInfo">
+                            <div className="user__itemName">Name</div>
+                            <div className="user__itemPrice">Price</div>
+                            <div className="user__itemLocation">Location</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 
-export default User;
+function mapStateToProps(state){
+    return{
+        user: state.user
+
+    }
+}
+
+export default connect(mapStateToProps,{getUser})(User);
