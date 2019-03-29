25520f25713fc072310663d44c39bf09ce67f340
Cobyswan <Cobyswan@yahoo.com>
2019-03-29 14:21:11 -0700

    Navbar2 updated

--- src/components/NavBar/NavBar2.js
+++ src/components/NavBar/NavBar2.js
@@ -25,7 +25,6 @@ class NavBar extends Component {
       this.props.getUser(res.data);
     });
   };
-
   // componentDidUpdate = (prevProps) => {
   //   if(this.props.filteredListings !== prevProps.filteredListings){
   //     // this.search
@@ -62,66 +61,93 @@ class NavBar extends Component {
       this.props.getUser(res.data);
     });
   };
-
   handleOpen = () => this.setState({ modalOpen: true });
-
   handleClose = () => this.setState({ modalOpen: false });
-
   render() {
+    let uiMenuStyle = {
+      marginBottom: 0,
+      backgroundColor: `rgb(55, 72, 92)`,
+      height: 60,
+      borderRadius: 0,
+      text: "white"
+    };
+    let inputStyle = {
+      height: 25,
+      width: 300,
+      alignSelf: "center",
+      borderBottomRightRadius: 0,
+      borderTopRightRadius: 0,
+    };
+    let searchButtonStyle = {
+      height: 25,
+      fontSize: 10,
+      borderTopLeftRadius: 0,
+      borderBottomLeftRadius: 0
+    };
     console.log(this.state.searchFilter);
     return (
-      <div className="navBar">
-        <div className="navBar__title">
-          <Link to="/">
-            <h1>MusicHub</h1>
+      <div>
+        <div className="ui menu" style={uiMenuStyle}>
+          <Link to="/" className="header item">
+            <h1 style={{ color: "white", fontSize: 28 }}>MusicHub</h1>
           </Link>
-        </div>
-        <div className="navBar__navInput">
-          <input
-            onChange={e => this.updateSearch(e.target.value)}
-            placeholder="Search MusicHub"
-          />
-          <Link to="/search_results">
-            <button
-              className="navBar__searchButton"
-              onClick={() => this.searchBarGlobal()}
-            >
-              Search
-            </button>
-          </Link>
-        </div>
-        <div className="navBar__smallNavs">
+          <div className="ui input" style={inputStyle}>
+            <input
+              style={inputStyle}
+              onChange={e => this.updateSearch(e.target.value)}
+              placeholder="Search MusicHub"
+            />
+            <Link to="/search_results">
+              <button
+                style={searchButtonStyle}
+                className="ui secondary button"
+                onClick={() => this.searchBarGlobal()}
+              >
+                Search
+              </button>
+            </Link>
+          </div>
+          <div className="right menu">
           <Modal
-            trigger={
+              trigger={
+                <Link onClick={this.handleOpen} className="item" style={{ color: "white" }} to="#">
+                  Sell
+                </Link>
+              }
+              open={this.state.modalOpen}
+              onClose={this.handleClose}
+            >
+              <Header>Upload an item</Header>
+              <Modal.Content>
+                <SellModal close={this.handleClose}/>
+              </Modal.Content>
+            </Modal>
+            <Link style={{ color: "white" }} className="item" to="/messages">
+              Messages
+            </Link>
+            {!this.props.user ? (
               <Link
-                onClick={this.handleOpen}
+                to="#"
                 className="item"
                 style={{ color: "white" }}
-                to="#"
+                onClick={() => this.login()}
               >
-                Sell
+                Login
               </Link>
-            }
-            open={this.state.modalOpen}
-            onClose={this.handleClose}
-          >
-            <Header>Upload an item</Header>
-            <Modal.Content>
-              <SellModal close={this.handleClose} />
-            </Modal.Content>
-          </Modal>
-
-          <Link to="/messages">
-            <h2>Messages</h2>
-          </Link>
-          {!this.props.user ? (
-            <button onClick={() => this.login()}>Login</button>
-          ) : (
-            <Link to="/">
-              <h2>{this.props.user.profile_name}</h2>
-              <button onClick={() => this.logout()}>Logout</button>
-            </Link>
-          )}
+            ) : (
+              <Link
+                className="item"
+                to="/"
+                style={{ color: "white" }}
+                onClick={() => this.logout()}
+              >
+                <h2 style={{ color: "white" }}>
+                  {this.props.user.profile_name}
+                </h2>
+                Logout
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     );
